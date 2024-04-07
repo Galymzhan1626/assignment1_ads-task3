0bb212296557b249ce46b3fc844523edfdaaf058
Galymzhan1626 <154108707+Galymzhan1626@users.noreply.github.com>
2024-04-08 01:49:00 +0500

    task3.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
+/**
+ * The Main class contains methods to check if a number is prime.
+ */
  class Main {
-     // Returns true if n is prime, else return false.
+      /**
+     * This method checks if a number 'n' is prime.
+     * It uses recursion to test divisibility starting from the integer 'i'.
+     * The base cases handle when 'n' is less than or equal to 2 and when 'i' squared is greater than 'n'.
+     * If 'n' is divisible by 'i', it is not prime. If 'i' squared is greater than 'n', 'n' is prime.
+     *
+     * @param n The integer to check for primality.
+     * @param i The current divisor being tested, starting with 2.
+     * @return true if 'n' is prime, false otherwise.
+     */
      static boolean isPrime(int n, int i) {
          // Base cases
          if (n <= 2)
